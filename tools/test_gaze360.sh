4b6714e39c1f941e123731bd15f58e7596ee0537
Zhuoguang Chen <121424566+zgchen33@users.noreply.github.com>
2023-12-12 13:46:10 +0800

    Update test_gaze360.sh

--- tools/test_gaze360.sh
+++ tools/test_gaze360.sh
@@ -1,4 +1,4 @@
 #！/bin/bash
 
 CUDA_VISIBLE_DEVICES="7" python tools/test_gaze360_gaze.py configs/multiclue_gaze/multiclue_gaze_r50_gaze360.py ckpts/multiclue_gaze_r50_gaze360.pth --json /data/gaze360/test.json --root /data/gaze360/test_rawframes/
-python tools/calculate_mae_gaze360.py --evafile results/results_multiclue_gaze_r50_gaze360_test.json
+python tools/calculate_mae_gaze360.py --evalfile results/results_multiclue_gaze_r50_gaze360_test.json
